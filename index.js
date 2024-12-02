7fa06740adea9f2d09b8ccbf2f2491ec268a62c2
24LP2027 24LP2027 <24LP2027@G301-CC0188.local>
2024-12-02 17:25:09 +0800

    batmunkhxo

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hello from variable")
+// 1. "Comments can make code readable" - нэг мөртэй коммент болгох
+// 2. Variable.js нэртэй файл үүсгэх, number, string, boolean, undefined, null зэрэг утгуудыг өгөх
+// 3. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined, null гэх мэт
+// 4. Утга оноогоогүй хувьсагч зарлах
+// 5. Утга оноосон хувьсагч зарлах
+// 6. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+// 7. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+const firstName = "Bilegbat"
+console.log(firstName)
+const lastName = "Batmunkh"
+console.log(lastName)
+const maritalStatus = "False"
+console.log(maritalStatus)
+const country = "Mongolia"
+console.log(country)
+const age = "20"
+console.log(age)
+
